5c3ebd1a3dba3fb49a3725ff62c44a1279a4ecf0
Sergio Palacios <sergioarepalacios9264@gmail.com>
2024-10-06 22:33:29 -0600

    Seguimiento de componente para metodos de pago

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,26 +1,53 @@
 import HeaderNav from "./HeaderNav"
 import "../assets/css/Payment.css"
 import { useState } from "react"
+import FooterN from "./footer";
 
 export default function PaymentMethods({ cart, convertPrice, totalPrice }) {
     const [paymentMethod, setPaymentMethod] = useState("");
+    const OnPlaceForm = () => {
+        return (
+            <>
+                <h2 className="h2">Información de contacto</h2>
+                <form>
+                    <div className="mb-3 form-floating">
+                        <input type="text" className="form-control" placeholder="Nombre" id="nombre" aria-describedby="nombre_user" required></input>
+                        <label htmlFor="nombre">Nombre</label>
+                        <p className="form-text">Su información no será compartida a terceros</p>
+                    </div>
+                    <div className="mb-3 form-floating">
+                        <input type="email" className="form-control" placeholder="name@example.com" id="email" aria-describedby="email_user" required></input>
+                        <label htmlFor="email">Correo electrónico</label>
+                        <p className="form-text">Su información no será compartida a terceros</p>
+                    </div>
+                    <div className="mb-3 form-floating">
+                        <input type="tel" className="form-control" placeholder="0000-0000" id="telefono" aria-describedby="phone_user" required></input>
+                        <label htmlFor="telefono">Número de teléfono</label>
+                        <p className="form-text">Su información no será compartida a terceros</p>
+                    </div>
+                    <div className="mb-3">
+                        <input type="submit" className="btn btn-info" value={"Guardar información"}></input>
+                    </div>
+                </form>
+            </>
+        )
+    }
     const handleClickPaymentOnPlace = () => {
-        setPaymentMethod("En el lugar")
+        setPaymentMethod(<OnPlaceForm />)
     }
     const handleClickPaymentOnline = () => {
         setPaymentMethod("En linea")
     }
-
     return (
         <>
             <header>
                 <HeaderNav title={'Zona de pago'} />
             </header>
             <main>
-                <section className="container text-center">
-                    <article className="card m-3">
+                <section className="container">
+                    <article className="card text-center m-3">
                         <div className="card-body">
-                            <div className="row row-payment d-flex align-items-center">
+                            <div className="row row-payment">
                                 <div className="col-6">
                                     <strong>Total a pagar: </strong>
                                 </div>
@@ -39,22 +66,22 @@ export default function PaymentMethods({ cart, convertPrice, totalPrice }) {
                         <main>
                             <section>
                                 <article className="row">
-                                    <div className="col-lg-3">
-                                        <div className="row flex-column">
-                                            <div className="col-6 d-flex justify-content-start">
-                                                <div className="row">
-                                                    <button className="btn btn-dark btn-lg" onClick={handleClickPaymentOnPlace}>Pago en el lugar</button>
+                                    <div className="col-md-12 col-lg-12 mb-3">
+                                        <div className="row row-btn-payment">
+                                            <div className="col-6">
+                                                <div className="row mb-2 g-3">
+                                                    <button className="btn btn-outline-dark btn-lg" onClick={handleClickPaymentOnPlace}>Pago en el lugar</button>
                                                 </div>
                                             </div>
-                                            <div className="col-6 mt-2 d-flex justify-content-start">
-                                                <div className="row">
-                                                    <button className="btn btn-dark btn-lg" onClick={handleClickPaymentOnline}>Pago en línea</button>
+                                            <div className="col-6">
+                                                <div className="row g-3">
+                                                    <button className="btn btn-outline-dark btn-lg" onClick={handleClickPaymentOnline}>Pago en línea</button>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="col-lg-9">
-
+                                    <div className="col-md-12 col-lg-9">
+                                        {paymentMethod}
                                     </div>
                                 </article>
                             </section>
@@ -62,6 +89,7 @@ export default function PaymentMethods({ cart, convertPrice, totalPrice }) {
                     </article>
                 </section>
             </main>
+            <FooterN />
         </>
     )
 }
